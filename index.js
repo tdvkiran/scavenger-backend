b7c2f027a5bb602089725ca0aa85c8d2bee43fda
tkiran-mdsol <tkiran@mdsol.com>
2021-05-30 00:16:29 +0530

    fix cors issue

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ mongoose.connect(process.env.MONGODB_URL,{
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 routes(app);
 
 app.get('/',(req,res)=>{
